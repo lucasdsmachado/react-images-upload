041c2af56b4bad43c04320ca12e25ba83b445ebd
lucasdsmachado <briandsmachado@gmail.com>
2022-05-11 22:45:06 -0300

    complete cardlist component

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,29 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { onOpen, onClose, isOpen } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [imageUrl, setImageUrl] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  function handleViewImage(url: string) {
+    setImageUrl(url);
+    onOpen();
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
-
+      <SimpleGrid columns={3} spacing="40px">
+        {cards.map(card => (
+          // eslint-disable-next-line react/jsx-no-bind
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} imgUrl={imageUrl} onClose={onClose} />
     </>
   );
 }
